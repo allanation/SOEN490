e3ee8e2a207e9c181b63de9d2cf00d60c4344a34
mohona6646 <60407634+mohona6646@users.noreply.github.com>
2022-10-13 17:42:42 -0400

    -Add admin dashboard

--- WhatsUp/web/frontend/src/components/dashboard.js
+++ WhatsUp/web/frontend/src/components/dashboard.js
@@ -1,14 +1,41 @@
 import { useAuthState } from "react-firebase-hooks/auth";
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import { auth } from "../firebase";
+import searchloop from '../images/searchloop.png';
+import filter from '../images/filter.png';
+import "./dashboard.css";
 
 function Dashboard() {
-  const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth); 
+  const [isApproved, setColorApproved] = useState(false);
+  const [isUnApproved, setColorUnapproved] = useState(false);
+  const navigate = useNavigate();
+
+  function logOut(){
+    localStorage.clear();
+    navigate("/");
+  }
 
   return (
-    <>
-      <h1>Welcome {user?.email}</h1>
-      <button onClick={() => auth.signOut()}>Sign Out</button>
-    </>
+    <div className="div">
+      <span id="grey-box">
+        <div className="buttons">
+        <button className="Approved" onClick={()=>{setColorApproved(true); setColorUnapproved(false)}} style={{textDecoration: isApproved==true?"underline":"",textDecorationColor:isApproved==true?"#3acfb7":"",
+         textDecorationThickness: isApproved==true?"1.5px":"", textUnderlineOffset: isApproved==true?"1em":"", color: isApproved==true?"black":""}}>Approved</button>
+        <button className="Unapproved" onClick={()=>{setColorUnapproved(true); setColorApproved(false)}} style={{textDecoration: isUnApproved==true?"underline":"",textDecorationColor:isUnApproved==true?"#3acfb7":"",
+         textDecorationThickness: isUnApproved==true?"1.5px":"", textUnderlineOffset: isUnApproved==true?"1em":"", color: isUnApproved==true?"black":""}}>Unapproved</button>
+         <button className="logOut" onClick={logOut}>Log Out</button>
+        </div>
+        <div class="container">
+          <form id="search">
+            <input type="text" placeholder="Search for..." className="searchBar"></input>
+            <button type="submit" className="searchLoop"><img src={searchloop} width="40" height="40"></img></button>
+            </form>
+            <button className="filter"><img src={filter}></img></button>
+        </div>
+      </span>
+    </div>
   );
 }
 
