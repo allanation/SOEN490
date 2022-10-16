9c4a7f86c33526c2fde0eaf5866fad6ee614cd30
ahmadhany99 <ahmadhany1999@hotmail.com>
2022-10-16 15:09:04 -0400

    Lastest version

--- WhatsUp/app/screens/login.js
+++ WhatsUp/app/screens/login.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image, Alert } from "react-native";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Links from "../components/Links";
@@ -8,10 +9,46 @@ import ScreenSubtitle from "../components/ScreenSubtitle";
 import ScreenTitle from "../components/ScreenTitle";
 import logo from "../Images/w3.png";
 import AntDesign from "react-native-vector-icons/AntDesign";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+//import { useNavigation } from "@react-navigation/core";
 
 export default function Login() {
+  // const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        Alert.alert(
+          "Logged in sucessfully",
+          "Email and password are valid",
+          [{ text: "OK", onPress: () => console.log("") }],
+          { cancelable: false }
+        );
+        // navigation.navigate("dashboard");
+      })
+      .catch((error) => {
+        Alert.alert(
+          "Try again",
+          "Invalid email or password.",
+          [{ text: "OK", onPress: () => console.log("") }],
+          { cancelable: false }
+        );
+      });
+  };
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        //navigation.replace("Home");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
   return (
-    <Screen>
+    <Screen style={{ padding: 10, marginTop: 5 }}>
       <Image
         source={logo}
         style={{ width: 166, height: 212, alignSelf: "center" }}
@@ -21,9 +58,15 @@ export default function Login() {
       <AppTextInput
         placeholder="Email"
         keyboardType="email-address"
-        secureTextEntry={false}
+        onChangeText={(text) => setEmail(text)}
+        value={email}
       />
-      <AppTextInput placeholder="Password" secureTextEntry={true} />
+      <AppTextInput
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
       <View style={styles.organizertwo}>
         <Links
           style={styles.link}
@@ -31,7 +74,7 @@ export default function Login() {
           onPress={() => console.log("password")}
         />
       </View>
-      <AppButton style title="Login" onPress={() => console.log("SOEN490")} />
+      <AppButton style title="Login" onPress={handleLogin} />
       <View style={{ flexDirection: "row", justifyContent: "center" }}>
         <Text style={styles.text}>
           Don't have an account?
@@ -46,17 +89,17 @@ export default function Login() {
         style={{
           flexDirection: "row",
           alignItems: "center",
-          paddingVertical: 30,
+          paddingVertical: 15,
         }}
       >
         <View style={{ flex: 1, height: 1, backgroundColor: "grey" }} />
         <View>
-          <Text style={{ width: 90, textAlign: "center" }}>Or login with</Text>
+          <Text style={{ width: 105, textAlign: "center" }}>Or login with</Text>
         </View>
         <View style={{ flex: 1, height: 1, backgroundColor: "grey" }} />
       </View>
-      {/* <View style={styles.roundshape}>
-        <AntDesign
+      <View style={styles.rowContainer}>
+        <FontAwesome
           name="facebook-square"
           size={30}
           style={{
@@ -66,7 +109,26 @@ export default function Login() {
           }}
           onPress={() => console.log("Facebook")}
         />
-      </View> */}
+        <FontAwesome
+          name="twitter-square"
+          size={30}
+          style={{
+            color: "#00acee",
+            fontSize: 50,
+            paddingHorizontal: 10,
+          }}
+          onPress={() => console.log("Facebook")}
+        />
+        <FontAwesome
+          name="google"
+          size={30}
+          style={{
+            color: "#db4a39",
+            fontSize: 50,
+          }}
+          onPress={() => console.log("Facebook")}
+        />
+      </View>
     </Screen>
   );
 }
