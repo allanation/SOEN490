5507389843b4d8e0042b403f18e031832a55ba3c
allan <48453731+allanation@users.noreply.github.com>
2022-10-19 23:29:50 -0400

    event tabs and search bar works for each tab
    -need to fix: refresh the events when clicking on upcoming and previous with the respective arrays

--- WhatsUp/app/screens/OrganizerDashboard.js
+++ WhatsUp/app/screens/OrganizerDashboard.js
@@ -31,18 +31,19 @@ function OrganizerDashboardScreen() {
     let date = today.getMonth() + 1 + '-' + today.getDate();
     setDate(date);
     setMasterData(events);
+    setPreviousData(eventsPast);
   }, []);
 
   const events = [
     {
       image: '../assets/Logos/w1.png',
-      title: 'Slay of the Week',
-      organizer: 'Ari',
+      title: 'Fashion Week',
+      organizer: 'Lasalle College',
       date: 'May 21, 2020',
     },
     {
       image: { EventImage },
-      title: 'Orientation Semaine',
+      title: 'Orientation Week',
       organizer: 'ETS',
       date: 'May 21, 2022',
     },
@@ -54,8 +55,8 @@ function OrganizerDashboardScreen() {
     },
     {
       image: { EventImage },
-      title: 'Orientation Week',
-      organizer: 'mcgill',
+      title: 'Film Festival',
+      organizer: 'Cinema',
       date: 'May 21, 2024',
     },
   ];
@@ -63,51 +64,36 @@ function OrganizerDashboardScreen() {
   const eventsPast = [
     {
       image: '../assets/Logos/w1.png',
-      title: 'HOE',
+      title: 'Sports Weekend',
       organizer: 'Concordia University',
       date: 'May 21, 2022',
     },
     {
       image: { EventImage },
-      title: 'Orientation Week',
+      title: 'Music Festival',
       organizer: 'Concordia University',
       date: 'May 21, 2022',
     },
     {
       image: { EventImage },
-      title: 'Orientation Week',
+      title: 'Jazz Festival',
       organizer: 'Concordia University',
       date: 'May 21, 2022',
     },
     {
       image: { EventImage },
-      title: 'Orientation Week',
-      organizer: 'Concordia University',
-      date: 'May 21, 2022',
-    },
-    {
-      image: { EventImage },
-      title: 'Orientation Week',
-      organizer: 'Concordia University',
-      date: 'May 21, 2022',
-    },
-    {
-      image: { EventImage },
-      title: 'Orientation Week',
+      title: 'F1 ',
       organizer: 'Concordia University',
       date: 'May 21, 2022',
     },
   ];
 
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [searchItems, setSearchItems] = React.useState([]);
-  const [eventView, setEventView] = useState(true);
+  const [displayedEvent, setDisplayedEvents] = useState(true);
   const [search, setSearch] = useState('');
   const [masterData, setMasterData] = useState([]);
+  const [previousData, setPreviousData] = useState([]);
   const [filteredData, setFilteredData] = useState('');
-  // const ItemView = ({ item }) => {
-  //   return <Text>Good Parts</Text>;
-  // };
+  const [filteredOrgData, setFilteredOrgData] = useState('');
 
   const ItemView = ({ item }) => {
     return (
@@ -120,16 +106,8 @@ function OrganizerDashboardScreen() {
     );
   };
 
-  // const ItemSeparatorView = () => {
-  //   return (
-  //     <View>
-  //       <Text>i'm separating</Text>
-  //     </View>
-  //   );
-  // };
-
   const searchFilter = (text) => {
-    if (text) {
+    if (text && displayedEvent) {
       const newData = masterData.filter((item) => {
         const itemData = item.title
           ? item.title.toUpperCase()
@@ -138,22 +116,122 @@ function OrganizerDashboardScreen() {
         return itemData.indexOf(textData) > -1;
       });
 
+      const orgSearch = masterData.filter((item) => {
+        const itemData = item.organizer
+          ? item.organizer.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+
+      setFilteredOrgData(orgSearch);
+      setFilteredData(newData);
+      console.log(filteredData);
+      setSearch(text);
+    } else if (text && !displayedEvent) {
+      const newData = previousData.filter((item) => {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+
+      const orgSearch = previousData.filter((item) => {
+        const itemData = item.organizer
+          ? item.organizer.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+
+      setFilteredOrgData(orgSearch);
       setFilteredData(newData);
       console.log(filteredData);
       setSearch(text);
     } else {
-      setFilteredData(masterData);
+      displayedEvent
+        ? setFilteredData(masterData)
+        : setFilteredData(previousData);
       setSearch(text);
     }
   };
 
+  const toggleDisplay = (e) => {
+    setDisplayedEvents({ displayedEvent: !displayedEvent });
+  };
+
+  var dE;
+  var tabs;
+  var bruh;
+  if (displayedEvent) {
+    tabs = (
+      <>
+        <TouchableOpacity
+          title='Show Form 1'
+          onPress={() => setDisplayedEvents(true)}
+          style={styles.upcoming}
+        >
+          <Text>upcoming</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          title='Show Form 2'
+          onPress={() => setDisplayedEvents(false)}
+          style={styles.previous}
+        >
+          <Text>previous</Text>
+        </TouchableOpacity>
+      </>
+    );
+    bruh = (
+      <>
+        <FlatList
+          data={filteredData ? filteredData : events}
+          renderItem={ItemView}
+        ></FlatList>
+        <FlatList
+          data={filteredOrgData ? filteredOrgData : []}
+          renderItem={ItemView}
+        ></FlatList>
+      </>
+    );
+  } else {
+    tabs = (
+      <>
+        <TouchableOpacity
+          title='Show Form 1'
+          onPress={() => setDisplayedEvents(true)}
+          style={styles.previous}
+        >
+          <Text>upcoming</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          title='Show Form 2'
+          onPress={() => setDisplayedEvents(false)}
+          style={styles.upcoming}
+        >
+          <Text>previous</Text>
+        </TouchableOpacity>
+      </>
+    );
+    bruh = (
+      <>
+        <FlatList
+          data={filteredData ? filteredData : eventsPast}
+          renderItem={ItemView}
+        ></FlatList>
+        <FlatList
+          data={filteredOrgData ? filteredOrgData : []}
+          renderItem={ItemView}
+        ></FlatList>
+      </>
+    );
+  }
   // for past and upcoming events use useStates and a conditional mapping
   return (
     <SafeAreaView>
       <ScrollView>
-        <Screen
-          style={{ padding: 20, marginTop: 20, backgroundColor: '#F5F5F5' }}
-        >
+        <Screen style={{ padding: 20, backgroundColor: '#F5F5F5' }}>
           <View style={styles.header}>
             <View style={styles.headerContent}>
               <Text style={{ color: colors.darkGrey }}>
@@ -186,11 +264,12 @@ function OrganizerDashboardScreen() {
               <Ionicons name='ios-filter' size={24} color={colors.primary} />
             </TouchableOpacity>
           </View>
+
+          <View style={styles.eventTabs}>{tabs}</View>
+
           <Text style={styles.eventTitle}>Your Events</Text>
-          <FlatList
-            data={filteredData ? filteredData : events}
-            renderItem={ItemView}
-          ></FlatList>
+
+          {bruh}
         </Screen>
       </ScrollView>
     </SafeAreaView>
@@ -222,6 +301,19 @@ const styles = StyleSheet.create({
   },
   filter: {
     justifyContent: 'center',
+  },
+  eventTabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 20,
+  },
+  upcoming: {
+    borderBottomWidth: 2,
+    borderBottomColor: colors.primary,
+  },
+  previous: {
+    borderBottomWidth: 2,
+    borderBottomColor: colors.darkGrey,
   },
 });
 
