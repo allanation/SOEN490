40e837a686d99f56e242e311c8a2649f3891d059
allan <48453731+allanation@users.noreply.github.com>
2022-11-05 12:43:17 -0400

    WHATSUP-8 Tags delete onPress

--- WhatsUp/app/App.js
+++ WhatsUp/app/App.js
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='DateInfo'
+        initialRouteName='OrgTags'
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name='Login' component={Login} />
