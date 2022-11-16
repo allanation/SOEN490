0caeab24900efb1fdb9d80fb3a921541747ccc38
allan <48453731+allanation@users.noreply.github.com>
2022-11-15 21:08:04 -0500

    WHATSUP-8 Fixed event tags

--- WhatsUp/app/components/EventTagsList.js
+++ WhatsUp/app/components/EventTagsList.js
@@ -4,15 +4,17 @@ import EventTags from './EventTags';
 
 export default function EventTagsList({ tags, onRemove }) {
   return (
-    <ScrollView style={{ flexDirection: 'row' }}>
-      {tags.map((tag) => (
-        <EventTags
-          tagname={tag.tagname}
-          key={tag.id}
-          {...tag}
-          onRemove={onRemove}
-        />
-      ))}
+    <ScrollView>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {tags.map((tag) => (
+          <EventTags
+            tagname={tag.tagname}
+            key={tag.id}
+            {...tag}
+            onRemove={onRemove}
+          />
+        ))}
+      </View>
     </ScrollView>
   );
 }
