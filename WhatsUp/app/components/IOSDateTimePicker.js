6bee115be8d6f28899eed46b87967de435baac79
allan <48453731+allanation@users.noreply.github.com>
2022-11-23 22:57:20 -0500

    WHATS-UP-8 IOS DateTimePicker validation

--- WhatsUp/app/components/IOSDateTimePicker.js
+++ WhatsUp/app/components/IOSDateTimePicker.js
@@ -1,43 +1,65 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import colors from "../config/colors";
-import AppButton from "./AppButton";
-
-
-
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import colors from '../config/colors';
+import AppButton from './AppButton';
 
 function IOSDateTimePicker() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [today, setToday] = useState(new Date());
+
+  const handleSelectStartDate = (e, date) => {
+    setStartDate(e.nativeEvent.timestamp);
+    setToday(date);
+  };
+
+  const handleSelectEndDate = (e) => {
+    setEndDate(e.nativeEvent.timestamp);
+  };
+
   return (
     <View>
       <Text style={styles.text}> Start Date & Time </Text>
       <View style={styles.picker}>
-        <DateTimePicker style={styles.btn} mode="date" value={new Date()} />
         <DateTimePicker
-          display="inline"
           style={styles.btn}
-          mode="time"
+          mode='date'
+          value={today}
+          onChange={handleSelectStartDate}
+          minimumDate={new Date()}
+        />
+        <DateTimePicker
+          display='inline'
+          style={styles.btn}
+          mode='time'
           value={new Date()}
-          minuteInterval="5"
+          minuteInterval='15'
         />
       </View>
       <View
         style={{
           flex: 1,
           height: 1,
-          width: "100%",
-          backgroundColor: "lightgrey",
+          width: '100%',
+          backgroundColor: 'lightgrey',
         }}
       />
       <Text style={styles.text}> End Date & Time </Text>
       <View style={styles.picker}>
-        <DateTimePicker style={styles.btn} mode="date" value={new Date()} />
         <DateTimePicker
-          display="inline"
           style={styles.btn}
-          mode="time"
+          mode='date'
+          value={today}
+          minimumDate={startDate}
+          onChange={handleSelectEndDate}
+        />
+        <DateTimePicker
+          display='inline'
+          style={styles.btn}
+          mode='time'
           value={new Date()}
-          minuteInterval="5"
+          minuteInterval='15'
         />
       </View>
     </View>
@@ -55,7 +77,7 @@ const styles = StyleSheet.create({
   },
   btn: {
     accentColor: 'red',
-    width: "40%",
+    width: '40%',
     marginBottom: 10,
     // backgroundColor: 'red'
   },
@@ -64,8 +86,8 @@ const styles = StyleSheet.create({
     color: colors.darkerGrey,
     marginTop: 10,
     fontSize: 24,
-    alignContent: "center",
-    alignSelf: "center",
+    alignContent: 'center',
+    alignSelf: 'center',
     flex: 1,
     flexDirection: 'row',
   },
