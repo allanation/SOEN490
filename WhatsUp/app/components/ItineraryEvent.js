7f8d3cce93c9766f7adf222c748a06407bf5b683
allan <48453731+allanation@users.noreply.github.com>
2022-11-27 14:00:07 -0500

    WHATSUP-8 Day Schedule Added edit and close functionality

--- WhatsUp/app/components/ItineraryEvent.js
+++ WhatsUp/app/components/ItineraryEvent.js
@@ -1,25 +1,134 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import colors from '../config/colors';
-
-function ItineraryEvent({ title, startTime, endTime, location, id, onRemove }) {
+import AppModal from './AppModal';
+import { Ionicons } from '@expo/vector-icons';
+import BackBtn from '../components/BackBtn';
+import { ScrollView } from 'react-native';
+import ScreenSubtitle from '../components/ScreenSubtitle';
+import ScreenTitle from '../components/ScreenTitle';
+import AppTextInput from './AppTextInput';
+import AppButton from './AppButton';
+import uuid from 'react-native-uuid';
+function ItineraryEvent({
+  title,
+  startTime,
+  endTime,
+  description,
+  location,
+  id,
+  onRemove,
+}) {
+  // const handleEdit = (e) => {
+  //   console.log('editing');
+  // };
+  const [itinerary, setItinerary] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [etitle, setETitle] = useState(title);
+  const [estartTime, setEStartTime] = useState(startTime);
+  const [eendTime, setEEndTime] = useState(endTime);
+  const [edescription, setEDescription] = useState(description);
+  const [elocation, setELocation] = useState(location);
+  const ids = uuid.v4();
+  function handleEdit(e) {
+    const editItinerary = {
+      title: title,
+      startTime: startTime,
+      endTime: endTime,
+      description: description,
+      location: location,
+      id: ids,
+    };
+    setItinerary((itinerary) => [...itinerary, editItinerary]);
+    setModalVisible(true);
+    console.log('editing', id);
+  }
   return (
-    <TouchableOpacity style={styles.itineraryButton} onPress={onRemove(id)}>
-      <View>
+    <TouchableOpacity style={styles.itineraryButton} onPress={handleEdit}>
+      <View style={styles.itineraryDetails}>
         <Text style={styles.itineraryTitle} numberOfLines={1}>
-          {title.length < 30 ? `${title}` : `${title.substring(0, 28)}...`}
+          {title.length < 30 ? `${etitle}` : `${etitle.substring(0, 28)}...`}
         </Text>
+        <Ionicons
+          name='close-outline'
+          size={24}
+          onPress={onRemove(id)}
+        ></Ionicons>
       </View>
       <View style={styles.itineraryDetails}>
         <Text style={{ color: colors.darkGrey }} numberOfLines={1}>
-          {startTime}-{endTime}
+          {estartTime}-{eendTime}
         </Text>
         <Text style={{ color: colors.darkGrey }} numberOfLines={1}>
-          {location.length < 16
-            ? `${location}`
-            : `${location.substring(0, 14)}...`}
+          {elocation.length < 16
+            ? `${elocation}`
+            : `${elocation.substring(0, 14)}...`}
         </Text>
       </View>
+      <AppModal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalView}>
+          <BackBtn
+            style={styles.backModal}
+            onPress={() => setModalVisible(!modalVisible)}
+          />
+          <View style={styles.inputView}>
+            <ScrollView
+              keyboardDismissMode='interactive'
+              style={{ width: '100%' }}
+            >
+              <ScreenTitle
+                style={{ alignSelf: 'center' }}
+                title={'Edit Item'}
+              />
+              <AppTextInput
+                placeholder='Title'
+                value={etitle ? etitle : console.log('no title')}
+                onChangeText={(currentTitle) => setETitle(currentTitle)}
+              />
+              <AppTextInput
+                placeholder='Start Time'
+                value={estartTime ? estartTime : console.log('no start time')}
+                onChangeText={(currentStartTime) =>
+                  setEStartTime(currentStartTime)
+                }
+              />
+              <AppTextInput
+                placeholder='End Time'
+                value={eendTime ? eendTime : console.log('no end time')}
+                onChangeText={(currentEndTime) => setEEndTime(currentEndTime)}
+              />
+              <AppTextInput
+                placeholder='Description'
+                value={
+                  edescription ? edescription : console.log('no description')
+                }
+                onChangeText={(currentDescription) =>
+                  setEDescription(currentDescription)
+                }
+              />
+              <AppTextInput
+                placeholder='Location (optional)'
+                value={elocation ? elocation : console.log('no location')}
+                onChangeText={(currentLocation) =>
+                  setELocation(currentLocation)
+                }
+              />
+              <AppButton
+                title='Edit'
+                style={{ marginTop: 0 }}
+                onPress={() => setModalVisible(false)}
+              />
+            </ScrollView>
+          </View>
+        </View>
+      </AppModal>
     </TouchableOpacity>
   );
 }
@@ -47,5 +156,52 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     color: colors.lightGrey,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 35,
+    padding: 20,
+    paddingTop: 20,
+    width: '86%',
+    // height: "62%",
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  inputView: {
+    marginTop: 8,
+    borderColor: colors.lightGrey,
+    borderRadius: 7,
+    width: '90%',
+    alignSelf: 'center',
+    paddingTop: 20,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    alignSelf: 'flex-start',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  backModal: {
+    backgroundColor: 'black',
   },
 });
--- WhatsUp/app/screens/OrgDaySchedule.js
+++ WhatsUp/app/screens/OrgDaySchedule.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   KeyboardAvoidingView,
   KeyboardAwareScrollView,
@@ -9,49 +9,49 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Pressable,
-} from "react-native";
-import Screen from "../components/Screen";
-import ScreenSubtitle from "../components/ScreenSubtitle";
-import ScreenTitle from "../components/ScreenTitle";
-import colors from "../config/colors";
-import { Ionicons } from "@expo/vector-icons";
-import AppButton from "../components/AppButton";
-import ItineraryEvent from "../components/ItineraryEvent";
-import EventTags from "../components/EventTags";
-import BackBtn from "../components/BackBtn";
-import AddBtn from "../components/AddBtn";
-import NewItemPopup from "../components/NewItemPopup";
-import AppModal from "../components/AppModal";
-import AppTextInput from "../components/AppTextInput";
-import { useNavigation } from "@react-navigation/native";
-import uuid from "react-native-uuid";
+} from 'react-native';
+import Screen from '../components/Screen';
+import ScreenSubtitle from '../components/ScreenSubtitle';
+import ScreenTitle from '../components/ScreenTitle';
+import colors from '../config/colors';
+import { Ionicons } from '@expo/vector-icons';
+import AppButton from '../components/AppButton';
+import ItineraryEvent from '../components/ItineraryEvent';
+import EventTags from '../components/EventTags';
+import BackBtn from '../components/BackBtn';
+import AddBtn from '../components/AddBtn';
+import NewItemPopup from '../components/NewItemPopup';
+import AppModal from '../components/AppModal';
+import AppTextInput from '../components/AppTextInput';
+import { useNavigation } from '@react-navigation/native';
+import uuid from 'react-native-uuid';
 
 function OrganizerDaySchedule({ day }) {
   useEffect(() => {
     const defaultItinerary = [
       {
-        title: "Round Table with William",
-        startTime: "9:00PM",
-        endTime: "10:00PM",
-        location: "Auditorium 101",
+        title: 'Round Table with William',
+        startTime: '9:00PM',
+        endTime: '10:00PM',
+        location: 'Auditorium 101',
       },
       {
-        title: "Round Table with William",
-        startTime: "9:00PM",
-        endTime: "10:00PM",
-        location: "Auditorium 101",
+        title: 'Round Table with William',
+        startTime: '9:00PM',
+        endTime: '10:00PM',
+        location: 'Auditorium 101',
       },
       {
-        title: "Round Table with William",
-        startTime: "9:00PM",
-        endTime: "10:00PM",
-        location: "Auditorium 101",
+        title: 'Round Table with William',
+        startTime: '9:00PM',
+        endTime: '10:00PM',
+        location: 'Auditorium 101',
       },
       {
-        title: "Round Table with William",
-        startTime: "9:00PM",
-        endTime: "10:00PM",
-        location: "Auditorium 101",
+        title: 'Round Table with William',
+        startTime: '9:00PM',
+        endTime: '10:00PM',
+        location: 'Auditorium 101',
       },
     ];
     setItinerary([]);
@@ -76,29 +76,30 @@ function OrganizerDaySchedule({ day }) {
     setItinerary(itinerary.filter((item) => item.id !== id));
   };
 
+  const displayEvents = () => {};
   const [itinerary, setItinerary] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
-  const [title, setTitle] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [description, setDescription] = useState("");
-  const [location, setLocation] = useState("");
+  const [title, setTitle] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
 
   const navigation = useNavigation();
 
   return (
     <Screen style={{ padding: 20, marginTop: 30 }}>
-      <View style={{ width: "100%", display: "flex" }}>
+      <View style={{ width: '100%', display: 'flex' }}>
         <ScreenTitle
-          style={{ alignSelf: "center" }}
-          title={"Day " + (day ? day + " " : "") + "schedule"}
+          style={{ alignSelf: 'center' }}
+          title={'Day ' + (day ? day + ' ' : '') + 'schedule'}
         />
         <ScreenSubtitle
-          style={{ alignSelf: "center" }}
-          subtitle="Please fill the following information"
+          style={{ alignSelf: 'center' }}
+          subtitle='Please fill the following information'
         />
       </View>
-      <BackBtn onPress={() => navigation.navigate("DateInfo")} />
+      <BackBtn onPress={() => navigation.navigate('DateInfo')} />
       <AddBtn onPress={() => setModalVisible(true)} />
       <ScrollView>
         <View style={{ marginTop: 12 }}>
@@ -130,54 +131,60 @@ function OrganizerDaySchedule({ day }) {
       </ScrollView>
       <View>
         <AppButton
-          title={"Next"}
-          onPress={() => navigation.navigate("OrgTags")}
+          title={'Next'}
+          onPress={() => navigation.navigate('OrgTags')}
         ></AppButton>
       </View>
       <AppModal
-        animationType="slide"
+        animationType='slide'
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
           setModalVisible(!modalVisible);
         }}
       >
-        <View  style={styles.modalView}>
-            <BackBtn style={styles.backModal} onPress={() => setModalVisible(!modalVisible)}/>
-            <View style={styles.inputView}>
-          <ScrollView keyboardDismissMode="interactive" style= {{width: "100%", }}>
-              <ScreenTitle style={{ alignSelf: "center" }} title={"New Item"} />
+        <View style={styles.modalView}>
+          <BackBtn
+            style={styles.backModal}
+            onPress={() => setModalVisible(!modalVisible)}
+          />
+          <View style={styles.inputView}>
+            <ScrollView
+              keyboardDismissMode='interactive'
+              style={{ width: '100%' }}
+            >
+              <ScreenTitle style={{ alignSelf: 'center' }} title={'New Item'} />
               <AppTextInput
-                placeholder="Title"
+                placeholder='Title'
                 onChangeText={(currentTitle) => setTitle(currentTitle)}
               />
               <AppTextInput
-                placeholder="Start Time"
+                placeholder='Start Time'
                 onChangeText={(currentStartTime) =>
                   setStartTime(currentStartTime)
                 }
               />
               <AppTextInput
-                placeholder="End Time"
+                placeholder='End Time'
                 onChangeText={(currentEndTime) => setEndTime(currentEndTime)}
               />
               <AppTextInput
-                placeholder="Description"
+                placeholder='Description'
                 onChangeText={(currentDescription) =>
                   setDescription(currentDescription)
                 }
               />
               <AppTextInput
-                placeholder="Location (optional)"
+                placeholder='Location (optional)'
                 onChangeText={(currentLocation) => setLocation(currentLocation)}
               />
               <AppButton
-                title="Add"
+                title='Add'
                 style={{ marginTop: 0 }}
                 onPress={handleAddingItinerary}
               />
-          </ScrollView>
-            </View>
+            </ScrollView>
+          </View>
         </View>
       </AppModal>
     </Screen>
@@ -187,14 +194,14 @@ function OrganizerDaySchedule({ day }) {
 const styles = StyleSheet.create({
   modalView: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 35,
     padding: 20,
     paddingTop: 20,
-    width: "86%",
+    width: '86%',
     // height: "62%",
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -207,27 +214,27 @@ const styles = StyleSheet.create({
     marginTop: 8,
     borderColor: colors.lightGrey,
     borderRadius: 7,
-    width: "90%",
-    alignSelf: "center",
+    width: '90%',
+    alignSelf: 'center',
     paddingTop: 20,
   },
   button: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    alignSelf: "flex-start",
+    alignSelf: 'flex-start',
   },
   buttonClose: {
-    backgroundColor: "#2196F3",
+    backgroundColor: '#2196F3',
   },
   textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center",
+    textAlign: 'center',
   },
   backModal: {
     backgroundColor: 'black',
