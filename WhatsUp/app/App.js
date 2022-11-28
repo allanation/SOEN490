610d9f0c607ea96bbb9416760d8721628a37c4de
sofiya <taversofiya@gmail.com>
2022-11-28 18:21:12 -0500

    change route

--- WhatsUp/app/App.js
+++ WhatsUp/app/App.js
@@ -52,7 +52,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='DateInfo'
+        initialRouteName='Login'
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name='Login' component={Login} />
