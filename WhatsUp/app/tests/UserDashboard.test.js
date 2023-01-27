c7f5bc678cd7fd5e17d993f19993168e266539f6
infernalturtle <jdiioia@outlook.com>
2023-01-26 23:14:37 -0500

    WHATSUP-159 Adding Test Coverage for User Dashboard
    Added tests and updated the User Dashboard for testing; fixed some bugs and removed useless code.

--- WhatsUp/app/tests/UserDashboard.test.js
+++ WhatsUp/app/tests/UserDashboard.test.js
@@ -8,16 +8,41 @@ import {
   screen,
 } from "@testing-library/react-native";
 import UserDashboardScreen from "../screens/UserDashboard";
+import LoginScreen from "../screens/Login";
+import UserProfileScreen from "../screens/UserProfile";
 
 jest.useFakeTimers();
 
-it("renders correctly", () => {
+it("Renders User Dashboard Correctly", () => {
   const tree = render(
     <NavigationContainer>
       <UserDashboardScreen />
     </NavigationContainer>
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("Logging in with attendee account should render User Dashboard Screen", async () => {
+  render(
+    <NavigationContainer>
+      <LoginScreen />
+    </NavigationContainer>
+  );
+  await waitFor(() =>
+    fireEvent.changeText(
+      screen.getByPlaceholderText("Email"),
+      "jd@test.com"
+    )
+  );
+  await waitFor(() =>
+    fireEvent.changeText(screen.getByPlaceholderText("Password"), "123456")
+  );
+  await waitFor(() => {
+    fireEvent.press(screen.getAllByText("Login")[1]);
+  });
+  render(<NavigationContainer>
+               <UserDashboardScreen />
+             </NavigationContainer>);
 });
 
 test("Clicking Location", async () => {
@@ -27,7 +52,7 @@ test("Clicking Location", async () => {
     </NavigationContainer>
   );
   await waitFor(() => {
-    fireEvent.press(screen.getByText("Montreal, QC"));
+    fireEvent.press(screen.getByTestId("location"));
   });
 });
 
@@ -37,9 +62,8 @@ test("Clicking Notification", async () => {
       <UserDashboardScreen />
     </NavigationContainer>
   );
-  const temp = screen.queryAllByText("");
   await waitFor(() => {
-    fireEvent.press(temp[0]);
+    fireEvent.press(screen.getByTestId("notification"));
   });
 });
 
@@ -51,18 +75,37 @@ test("Clicking Filter", async () => {
   );
   const temp = screen.queryAllByText("");
   await waitFor(() => {
-    fireEvent.press(temp[1]);
+    fireEvent.press(screen.getByTestId("filters"));
   });
 });
 
-test("Clicking Event", async () => {
+test("Searching in search bar events that include fa", async () => {
   render(
     <NavigationContainer>
       <UserDashboardScreen />
     </NavigationContainer>
   );
+  await waitFor(() => {
+    fireEvent.changeText(screen.getByPlaceholderText("Search for..."),"fa");
+  });
+});
+
+test("Clicking User Profile", async () => {
+  render(
+    <NavigationContainer>
+      <UserDashboardScreen />
+    </NavigationContainer>
+  );
+
   const temp = screen.queryAllByText("");
   await waitFor(() => {
-    fireEvent.press(screen.getByText("event"));
+    fireEvent.press(temp[5]);
   });
+
+  const userProfile = (
+    <NavigationContainer>
+      <UserProfileScreen />
+    </NavigationContainer>
+  );
+  render(userProfile);
 });
