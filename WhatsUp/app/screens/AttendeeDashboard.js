da8b83372ba0483027c85f4d51083e6364e2dda5
Andrea El-Hage <andreaelhage@hotmail.com>
2023-03-20 16:36:42 -0400

    WHATSUP-236: Removed old events from attendee dashboard

--- WhatsUp/app/screens/AttendeeDashboard.js
+++ WhatsUp/app/screens/AttendeeDashboard.js
@@ -64,15 +64,19 @@ function AttendeeDashboard() {
     const querySnapshot = await getDocs(q);
     if (querySnapshot != null) {
       querySnapshot.forEach((doc) => {
-        setAllEvents(
-          querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        );
-        setMasterData(
-          querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        );
-        setPreviousData(
-          querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        );
+        if(doc.data().endDate >= Date.now()){
+          console.log("heyy")
+          setAllEvents((allEvents) =>
+                  allEvents.concat({ id: doc.id, ...doc.data() })
+                );
+        setMasterData((allEvents) =>
+                  allEvents.concat({ id: doc.id, ...doc.data() })
+                );
+        setPreviousData((allEvents) =>
+                  allEvents.concat({ id: doc.id, ...doc.data() })
+                );
+
+        }
       });
     }
   };
@@ -217,7 +221,9 @@ function AttendeeDashboard() {
           </View>
           <Text style={styles.text}>Popular Events</Text>
         </View>
+        <View>
         {showEvents}
+        </View>
       </View>
     </Screen>
   );
